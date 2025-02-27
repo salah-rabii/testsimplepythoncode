cc2853f62607bc3f87a66de7726f1acb261e28a0
Salah eddine rabii <salahrabii2003@gmail.com>
2025-02-27 17:15:12 +0000

    ff

---
+++ calculate.py
@@ -0,0 +1,14 @@
+
+def addition(a, b):
+    return a + b
+
+def soustraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
---
+++ test_calculate.py
@@ -0,0 +1,34 @@
+import pytest
+# from code import addition, soustraction, multiplication, division
+# import code
+from calculate import addition, soustraction, multiplication, division
+# import /Users/salah/Downloads/simple_code_python_for_test/code
+
+
+
+
+
+def test_addition():
+    assert addition(2, 3) == 5
+    assert addition(-1, 1) == 0
+    assert addition(0, 0) == 0
+
+def test_soustraction():
+    assert soustraction(9, 7) == 2
+    assert soustraction(10, 4) == 6
+    assert soustraction(0, 0) == 0
+
+def test_multiplication():
+    assert multiplication(2, 3) == 6
+    assert multiplication(-1, 1) == -1
+    assert multiplication(0, 5) == 0
+
+def test_division():
+    assert division(6, 2) == 3
+    assert division(-6, 2) == -3
+    assert division(0, 5) == 0  # Zero divided by non-zero
+    assert division(5, 2) == 2.5  # Result is a float
+    assert division(-6, -2) == 3  # Both numbers negative
+    assert division(6, -2) == -3  # Positive divided by negative
+    with pytest.raises(ValueError):
+        division(1, 0)  # Division by zero
