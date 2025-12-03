0f96673634155b308196f579f7f35dd9cfa96cdd
Salah eddine rabii <salahrabii2003@gmail.com>
2025-12-03 16:03:07 +0100

    wrong code

--- test_calculate.py
+++ test_calculate.py
@@ -19,7 +19,7 @@ def test_soustraction():
     assert soustraction(0, 0) == 0
 
 def test_multiplication():
-    assert multiplication(2, 3) == 8
+    assert multiplication(2, 3) == 6
     assert multiplication(-1, 1) == -1
     assert multiplication(0, 5) == 0
 
